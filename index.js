91f1c2b9616b010a42a542787091a8265f6af8a1
waqasanwar0 <111072835+waqasanwar0@users.noreply.github.com>
2022-09-12 11:20:53 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hey Sir!')
+  response.send('Hey Waqas!')
 })
 
 app.listen(app.get('port'), function() {
